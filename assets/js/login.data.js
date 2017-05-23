100b17b500769f33b371e75e8ba3a5a348ed0449
ravitejapullari <raviteja.pullari@gmail.com>
2017-05-23 12:00:28 +0100

    Added my project

---
+++ assets/js/login.data.js
@@ -0,0 +1,29 @@
+var _logindata = [{
+        'username': 'raviteja',
+        'password': 'cts12345',
+        'data': {
+            'segmentid': 'kx12341'
+        }
+    },
+    {
+        'username': 'kishore',
+        'password': 'cts12345',
+        'data': {
+            'segmentid': 'kx12342'
+        }
+    },
+    {
+        'username': 'amit',
+        'password': 'cts12345',
+        'data': {
+            'segmentid': 'kx12343'
+        }
+    },
+    {
+        'username': 'rohit',
+        'password': 'cts12345',
+        'data': {
+            'segmentid': 'kx12344'
+        }
+    }
+];
---
+++ assets/js/login.js
@@ -0,0 +1,25 @@
+'use strict';
+
+_barclaysAdserver = {
+    _checklogin: function(k, l) {
+        var _userdata = _logindata;
+        for (var i = 0, j = _userdata.length; i < j; i++) {
+            if (_userdata[i]['username'] === k) {
+                if (_userdata[i]['password'] === l) {
+                    console.log(_userdata[i]['username']);
+                    $('#loginpage').hide();
+                    $('#sucessfull').show();
+                    _barclaysUser = _userdata[i]; // location.href = 'barclays-in.html';
+                    $('#b_username').html(_barclaysUser['username']);
+                }
+            }
+        }
+    },
+    _executeLogin: function() {
+        var _username = $('#username').val(),
+            _password = $('#userpassword').val();
+        if (_username != '' && _password != '') {
+            _barclaysAdserver._checklogin(_username, _password);
+        }
+    }
+}
