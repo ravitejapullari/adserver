1e185e26df8f9e125553c3df440dfff602ad6526
ravitejapullari <raviteja.pullari@gmail.com>
2017-06-08 12:07:51 +0100

    Added my project

--- assets/js/site.js
+++ assets/js/site.js
@@ -19,15 +19,15 @@ var _barclaysAdserver = _barclaysAdserver || {},
 var aimRnd = Math.round(Math.random() * 100000000);
 
 // adserver URL 
-var adserver = _sasObj.saswebsever + '/bserver';
+var adserver = _sasObj.saswebsever + '/bserver/';
 
 // Ad tag targeting values which will be appended to each ad request section in the bserver ad call 
-var allAdTags = "/ball/random=" + aimRnd + "/viewid=" + aimRnd;
+//var allAdTags = "/ball/random=" + aimRnd + "/viewid=" + aimRnd;
 
 // Individual tags for each ad request - increment the adx variable name and the ‘/bx/’ parameter. 
 var ad1 = "/b1/" + _sasObj.segment.FLIGHTID;
 
 // bserver ad call – insert the adx variables 
-document.write('<scr' + 'ipt src="' + adserver + allAdTags + ad1 + '?" type="text/JavaScript" language="JavaScript">');
+document.write('<scr' + 'ipt src="' + adserver + ad1 + '?" type="text/JavaScript" language="JavaScript">');
 document.write('</scr' + 'ipt>');
 // End Hide -->
