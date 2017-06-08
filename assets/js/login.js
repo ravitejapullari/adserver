04b9b4d24e3ff05c98cc5dff6fbe441ba13eb1cf
ravitejapullari <raviteja.pullari@gmail.com>
2017-06-08 11:16:08 +0100

    Added my project

--- assets/js/login.js
+++ assets/js/login.js
@@ -67,7 +67,7 @@
             _sasSegPar = _sasSegPar.join('/');
             var _srcimage = _kruxSegments.saswebsever + '/' +
                 _kruxSegments['adcallmethod'] + '/' + _sasSegPar;
-            $('#barclayscarloan').HTML('<script src=' + _srcimage + '></script>'); //_srcimage);
+            $('#barclayscarloan').append('<script src=' + _srcimage + '></script>'); //_srcimage);
         },
         _aemServCall: function() {
             _barclaysAdserver._adAjaxServ();
