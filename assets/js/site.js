61261aa848c9f5d0c5ebeddb9dc65ed577ba0ca7
ravitejapullari <raviteja.pullari@gmail.com>
2017-06-08 11:59:25 +0100

    Added my project

--- assets/js/site.js
+++ assets/js/site.js
@@ -13,3 +13,21 @@ var _barclaysAdserver = _barclaysAdserver || {},
         "adcallmethod": "jserver",
         "saswebsever": "https://crtlt2.aimatch.com/demo"
     };
+
+
+// Cache-busting and viewid value 
+var aimRnd = Math.round(Math.random() * 100000000);
+
+// adserver URL 
+adserver = _sasObj.saswebsever + '/bserver';
+
+// Ad tag targeting values which will be appended to each ad request section in the bserver ad call 
+allAdTags = "/ball/random=" + aimRnd + "/viewid=" + aimRnd;
+
+// Individual tags for each ad request - increment the adx variable name and the ‘/bx/’ parameter. 
+ad1 = "/b1/" + _sasObj.segment.FLIGHTID;
+
+// bserver ad call – insert the adx variables 
+document.write('<scr' + 'ipt src="' + adserver + allAdTags + ad1 + '?" type="text/JavaScript" language="JavaScript">');
+document.write('</scr' + 'ipt>');
+// End Hide -->
