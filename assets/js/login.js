45082edf6fa0dbe39a3661a94a0f8b8e02db58ec
ravitejapullari <raviteja.pullari@gmail.com>
2017-06-28 16:00:46 +0100

    Added my project

--- assets/js/login.js
+++ assets/js/login.js
@@ -57,9 +57,9 @@
         },
         _sasAdServCall: function(sData) {
             var _kruxSegments = sData.userdata['segments'],
-                _segmentpair = '';
-            var head = document.getElementsByTagName('head')[0];
-            _adserver = _sasObj.saswebsever + _sasObj.adcallmethod;
+                _segmentpair = '',
+                head = document.getElementsByTagName('head')[0],
+                _adserver = _sasObj.saswebsever + _sasObj.adcallmethod;
             for (var i = 0, j = _kruxSegments.ranks.length; i < j; i++) {
                 _segmentpair += 'b' + (i + 1) + '/Segments=' + _kruxSegments.ranks[i] + ',' + _kruxSegments.products[i] + '/Location_AccountType=' + _kruxSegments.accounttype + '/';
             }
