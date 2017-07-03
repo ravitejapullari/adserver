5097976bb6d1d0b53d6dd1a2c2efa74223702337
ravitejapullari <raviteja.pullari@gmail.com>
2017-07-03 13:53:19 +0100

    Added my project

--- assets/js/login.js
+++ assets/js/login.js
@@ -22,7 +22,7 @@
         _executeLogin: function() {
             var _uname = $('#username').val(),
                 _pword = $('#userpassword').val();
-            console.log(_uname);
+            console.log($);
             if (_uname != '' && _pword != '') {
                 _barclaysAdserver._checklogin(_uname, _pword);
             }
